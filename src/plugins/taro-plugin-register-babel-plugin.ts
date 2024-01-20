de4c41bbf6d4276c05602969eeb00d6bc4d3406f
yu.pan <panyu@gumingnc.com>
2024-01-21 06:02:01 +0800

    feat: update register

--- src/plugins/taro-plugin-register-babel-plugin.ts
+++ src/plugins/taro-plugin-register-babel-plugin.ts
@@ -1,11 +1,8 @@
 import type {IPluginContext} from "@tarojs/service";
 import path from 'path';
-import {AssetsToUrlOpt} from "./babel-plugin-assets-path-transform";
 
 
-module.exports = (ctx: IPluginContext, pluginOpts: AssetsToUrlOpt) => {
-
-    const {cacheFileName} = pluginOpts
+module.exports = (ctx: IPluginContext,) => {
 
     ctx.modifyWebpackChain(({chain}) => {
         const tempPath = path.resolve(__dirname, './babel-plugin-assets-to-url');
@@ -13,7 +10,7 @@ module.exports = (ctx: IPluginContext, pluginOpts: AssetsToUrlOpt) => {
         chain.module
             .rule('script')
             .use('babelLoader')
-            .options({plugins: [[tempPath, {cacheFileName}]]})
+            .options({plugins: [tempPath]})
 
     })
 
